3b1f9997dba84bf7050a8794f3279ace3ee6e881
Administrativo <administrativo@ncm.local>
2018-06-25 15:00:06 -0300

    fale conosco

--- views/faleconosco.php
+++ views/faleconosco.php
@@ -8,27 +8,6 @@
     <link rel="stylesheet" href="../css/style_fale_conosco.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript">
-
-    	//pega a largura da resolução da tela
-    	var width = screen.width;
-    	//pega a altura da resolução da tela
-    	var height = screen.height;
-      var coluna = null;
-
-    	//verifica se a resolução dará uma boa visão do site
-    	if (width > 520){
-        coluna = 70;
-        <?php $coluna =70; ?>
-
-    	}else if(width <= 520){
-
-        coluna = 40;
-        <?php $coluna =40; ?>
-        }
-
-
-    </script>
   </head>
   <body>
 
@@ -47,23 +26,24 @@
       <form class="" action="faleconosco.php" method="post">
         <div class="conteudo">
           <div class="suporte_fale_conos">
-            <div class="titulos_fale_conosco">NOME</div>
+            <div class="titulos_fale_conosco">Nome</div>
             <input type="text" name="txt_nome" value="" placeholder="digite seu nome" class="input_fale">
           </div>
 
           <div class="suporte_fale_conos">
-            <div class="titulos_fale_conosco">EMAIL</div>
+            <div class="titulos_fale_conosco">Email</div>
             <input type="text" name="txt_email" value="" placeholder="digite seu email" class="input_fale">
           </div>
 
           <div class="suporte_fale_conos">
-            <div class="titulos_fale_conosco">TELEFONE PARA CONTATO</div>
+            <div class="titulos_fale_conosco">Telefone para contato</div>
             <input type="text" name="txt_email" value="" placeholder="(11) 99999-4444" class="input_fale">
           </div>
 
           <div class="suporte_fale_conosco_mensagem">
-            <div class="titulos_fale_conosco">MENSAGEM</div>
-            <textarea name="txt_mensagem" rows="8" cols="<?php echo $coluna?>"></textarea>
+            <div class="titulos_fale_conosco">Mensagem</div>
+            <textarea name="txt_mensagem" rows="8" cols="70" class="txtarea"></textarea>
+            <textarea name="txt_mensagem" rows="8" cols="40" class="txtarea2"></textarea>
           </div>
 
           <div class="suporte_botao">
